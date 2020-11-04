724a065ace2493418ce4db26362d5a16f6220201
khailq-1062 <73331096+khailq-1062@users.noreply.github.com>
2020-11-04 10:06:34 +0700

    CHAPTER 12. PASSWORD RESET

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,12 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation user
     @user = user
-    mail to: user.email, subject: t".activation"
+    mail to: user.email, subject: t(".activation")
   end
 
-  def password_reset
-    @greeting = "Hi"
-
-    mail to: "to@example.org"
+  def password_reset user
+    @user = user
+    mail to: user.email, subject: t(".reset")
   end
 end
