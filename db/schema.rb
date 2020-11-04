44ec285d9a2d0d798691526ba51faa258e2f9eca
khailq-1062 <73331096+khailq-1062@users.noreply.github.com>
2020-11-04 10:06:34 +0700

    Chapter 6 : Modeling Users

--- db/schema.rb
+++ db/schema.rb
@@ -5,6 +5,6 @@ ActiveRecord::Schema.define(version: 2020_11_05_091334) do
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
-
 end
