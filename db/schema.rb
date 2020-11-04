b87f99ef697b81801734d8b87fbc354a17017bb0
khailq-1062 <73331096+khailq-1062@users.noreply.github.com>
2020-11-04 10:06:34 +0700

    CHAPTER 8. BASIC LOGIN

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,4 @@
 ActiveRecord::Schema.define(version: 2020_11_05_091334) do
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.string "email"
